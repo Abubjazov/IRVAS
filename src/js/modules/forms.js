e1616d50c83f7c0bb6e3c1cb26734b7feff61823
Radik Abubjazov <abubjazov@mail.ru>
2021-07-17 20:29:13 +0300

    добавлен скрипт forms для отправки форм на сайте

---
+++ src/js/modules/forms.js
@@ -0,0 +1,55 @@
+const forms = () => {
+
+    const form = document.querySelectorAll('form'),
+          inputs = document.querySelectorAll('input');
+
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся.',
+        failure: 'Извините, похоже что-то пошло не так... Обновите страницу и попробуйте ещё раз'
+    };
+
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+
+            const formData = new FormData(item);
+
+            postData('assets/server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    statusMessage.textContent = message.success;
+                })
+                .catch(() => statusMessage.textContent = message.failure)
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                });
+
+        });
+    });
+};
+
+export default forms;
